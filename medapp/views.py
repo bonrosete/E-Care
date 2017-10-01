0ec841de812db332a9bdeb2612636e6b4b7a781c
Bon Eric Rosete <boneric.rosete@gmail.com>
2017-10-01 23:47:49 +0800

    Semi Final

--- medapp/views.py
+++ medapp/views.py
@@ -87,7 +87,7 @@ def logout(request):
 	current_user.latest_logout = request.session['logout_time']
 	current_user.save()
 	request.session.clear()
-	return redirect('/')
+	return redirect('https://goo.gl/forms/HCmRo3UQS6MmZ06L2')
 
 
 def dashboard(request):
